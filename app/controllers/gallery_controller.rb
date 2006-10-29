0127f64567b1d58d9b96ec1c8c04ff43d31e172a
Sean O'Hara <sohara@sohara.com>
2006-10-29 20:53:15 +0000

    added gallery show action, changed layout, added top nav menu

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -13,4 +13,18 @@ class GalleryController < ApplicationController
     @gallery = Gallery.find(params[:id])
   end
   
+  def view_image
+    @image = Image.find(params[:id])
+    render :update do |page|
+      page[:innerbox].replace_html :partial => 'gallery/view_image' 
+      #page[:box].hide
+      page[:overlay].show
+      page[:box].setStyle :width => "" + (@image.thumbnails.detect{|t|t.thumbnail == "size800"}.width + 0).to_s + "px"
+      page[:box].setStyle :height => "" + (@image.thumbnails.detect{|t|t.thumbnail == "size800"}.height + 30).to_s + "px"
+      page << "center('box');"
+      page[:box].visual_effect :appear, :duration => 0.5
+      #page[:box].show
+    end
+  end
+  
 end
