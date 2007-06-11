969300a787e66d2ac0812b75daf756029feb4473
Sean O'Hara <sohara@sohara.com>
2007-06-11 15:15:04 +0000

    updated image viewing to display title

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -26,7 +26,7 @@ class GalleryController < ApplicationController
       #page[:box].hide
       page[:overlay].show
       page[:box].setStyle :width => "" + (@image.thumbnails.detect{|t|t.thumbnail == "size800"}.width + 0).to_s + "px"
-      page[:box].setStyle :height => "" + (@image.thumbnails.detect{|t|t.thumbnail == "size800"}.height + 30).to_s + "px"
+      page[:box].setStyle :height => "" + (@image.thumbnails.detect{|t|t.thumbnail == "size800"}.height + 30 + (@image.title_en.blank? ? 0 : 30)).to_s + "px"
       page << "center('box');"
       page[:box].visual_effect :appear, :duration => 0.5
       #page[:box].show
